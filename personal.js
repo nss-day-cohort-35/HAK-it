2465fda96841e4f98a46d7e8c38bc2a86c8ce8bd
Alex <o.a.momotyuk@gmail.com>
2019-08-21 15:37:36 -0500

    first version of js code for personal page

--- personal.js
+++ personal.js
@@ -1,5 +1,4 @@
 /*
-
 */
 
 var artists = [
@@ -12,17 +11,11 @@ var artists = [
         deathDate: [ "Death Date", "" ],
         countryOfOrigin: [ "Country of Origin", "U.S." ],
         countryOfCurrentResidence: [ "Country of Current Residence", "U.S." ],
-/*
-        List of immediate family members (for example: spouse, children, siblings, parents) - minimum 3 with images.
-        For multiple spouses, show begin/end dates.
-        */
-
         mother: [ "Mother", "Cynthia Louise" ],
         father: [ "Father", "Joseph Germanotta" ],
         siblings: [ "Sister", "Natali" ],
         children: [ "" ],
         spouses: [ "" ]
-
     }
 ]
 
@@ -30,73 +23,38 @@ const mainSection = document.querySelector( '.main-section__personal-data' );
 
 console.log( Object.keys( artists[0] ) );
 
-/*
-Object.keys( artists[0] ).forEach(item => {
-    if ( item.length != 0 ) {
-        console.log( item, artists[0].item ); */
-        /*
-        const divElement = document.createElement( 'div' );
-        divElement.innerHTML = artists[0].item[0] + ": " + artists[0].item[1];
-        mainSection.appendChild( divElement );*/
-/*    }
-});
-*/
+function addRecord ( item1,item2 ) {
+    const item = document.createElement( 'div' );
+    item.innerHTML = item1 + ": " + item2;
+    item.className="main-section__div";
+    mainSection.appendChild( item );
+}
 
-/*
-const itemName = document.createElement( 'div' );
-itemName.innerHTML = artists[0].itemName[0] + ": " + artists[0].itemName[1];
-mainSection.appendChild( itemName );
-*/
+function addPics( item1, item2, imgFileName, imgAlt ) {
+    const item = document.createElement( 'div' );
+    item.className="main-section__div";
+    const item_text = document.createElement( 'div' );
+    /*item_text.innerHTML = artists[0].item[0] + ": " + artists[0].item[1];
+*/    item_text.innerHTML = item1 + ": " + item2;
+    item_text.style = "text-align: justify-all";
+    item.appendChild( item_text );
+    const itemPics = document.createElement( 'img' );
+    itemPics.src = imgFileName;
+    itemPics.alt = imgAlt;
+    itemPics.style = "width: 100px; margin: .9em";
+    itemPics.className = "main-section__div-img";
+    item.appendChild( itemPics );
+    mainSection.appendChild( item );
+}
 
-const firstName = document.createElement( 'div' );
-firstName.innerHTML = artists[0].firstName[0] + ": " + artists[0].firstName[1];
-firstName.className="main-section__div";
-mainSection.appendChild( firstName );
+addRecord( artists[0].firstName[0], artists[0].firstName[1] );
+addRecord( artists[0].middleName[0], artists[0].middleName[1] );
+addRecord( artists[0].lastName[0], artists[0].lastName[1] );
+addRecord( artists[0].publicName[0], artists[0].publicName[1] );
+addRecord( artists[0].birthDate[0], artists[0].birthDate[1] );
+addRecord( artists[0].countryOfOrigin[0], artists[0].countryOfOrigin[1] );
+addRecord( artists[0].countryOfCurrentResidence[0], artists[0].countryOfCurrentResidence[1] );
 
-const middleName = document.createElement( 'div' );
-middleName.innerHTML = artists[0].middleName[0] + ": " + artists[0].middleName[1];
-middleName.className="main-section__div";
-mainSection.appendChild( middleName );
-
-const lastName = document.createElement( 'div' );
-lastName.innerHTML = artists[0].lastName[0] + ": " + artists[0].lastName[1];
-lastName.className="main-section__div";
-mainSection.appendChild( lastName );
-
-const publicName = document.createElement( 'div' );
-publicName.innerHTML = artists[0].publicName[0] + ": " + artists[0].publicName[1];
-publicName.className="main-section__div";
-mainSection.appendChild( publicName );
-
-const birthDate = document.createElement( 'div' );
-birthDate.innerHTML = artists[0].birthDate[0] + ": " + artists[0].birthDate[1];
-birthDate.className="main-section__div";
-mainSection.appendChild( birthDate );
-/*
-const deathDate = document.createElement( 'div' );
-deathDate.innerHTML = artists[0].deathDate[0] + ": " + artists[0].deathDate[1];
-mainSection.appendChild( deathDate );
-*/
-const countryOfOrigin = document.createElement( 'div' );
-countryOfOrigin.innerHTML = artists[0].countryOfOrigin[0] + ": " + artists[0].countryOfOrigin[1];
-mainSection.appendChild( countryOfOrigin );
-
-const countryOfCurrentResidence = document.createElement( 'div' );
-countryOfCurrentResidence.innerHTML = artists[0].countryOfCurrentResidence[0] + ": " + artists[0].countryOfCurrentResidence[1];
-countryOfCurrentResidence.className="main-section__div";
-mainSection.appendChild( countryOfCurrentResidence );
-
-const mother = document.createElement( 'div' );
-mother.innerHTML = artists[0].mother[0] + ": " + artists[0].mother[1];
-mother.className="main-section__div";
-mainSection.appendChild( mother );
-
-const father = document.createElement( 'div' );
-father.innerHTML = artists[0].father[0] + ": " + artists[0].father[1];
-father.className="main-section__div";
-mainSection.appendChild( father );
-
-const siblings = document.createElement( 'div' );
-siblings.innerHTML = artists[0].siblings[0] + ": " + artists[0].siblings[1];
-siblings.className="main-section__div";
-mainSection.appendChild( siblings );
+addPics( artists[0].mother[0], artists[0].mother[1], "images/ladyGagaMother.jpeg","Lady Gaga mother pics" );
+addPics( artists[0].father[0], artists[0].father[1], "images/ladyGagaFather.jpeg","Lady Gaga father pics" );
+addPics( artists[0].siblings[0], artists[0].siblings[1], "images/ladyGagaSister.jpeg","Lady Gaga sister pics" );
