4990f0d8fa704b9eb6671eca2a29ff043253ea8a
Alex <o.a.momotyuk@gmail.com>
2019-08-22 11:01:52 -0500

    newsfeed page js code was updated

--- newsfeed.js
+++ newsfeed.js
@@ -3,6 +3,16 @@
 
 var newsRecords = [
     {
+        date: ["Date", "December 10, 2018"],
+        location: ["Location", "Las Vegas"],
+        title: ["Title", "GRAMMY Award-winning Superstar Lady Gaga Adds Five Additional Jazz and Piano Shows to 2019 Schedule"],
+        message: ["Message", "Six-time GRAMMY Award-winner, Golden Globe Award-winner and Academy Award-nominated superstar entertainer Lady Gaga will add five additional LADY GAGA JAZZ & PIANO shows to her 2019 performance schedule at Park Theater. Presented in partnership by MGM Resorts International and Live Nation, the JAZZ & PIANO shows will feature stripped-down versions of her hits as well as music from the Great American Songbook. Lady Gaga previously announced four exclusive JAZZ & PIANO evenings as part of her Las Vegas residency, and due to overwhelming demand, she has decided to make them a permanent element of her Las Vegas performance schedule."],
+	    typeOfEvent: ["Type of event", "Show, Concert"],
+        collaborators: ["Collaborators", ""],
+        ticketLocation: ["Where to purchase tickets", "Ticketmaster outlets", "Ticketmaster.com", "GagaVegas.com" ],
+        image: ["ImageURL", "images/lady-gaga-logo.png"]
+    },
+    {
         date: [ "Date", "August 21, 2019" ],
         location: [ "Location", "Nashville, TN" ],
         title: [ "Title", "Lady GaGa is coming!" ],
@@ -11,7 +21,10 @@ var newsRecords = [
         collaborators: [ "Collaborators", "People of Tennessee" ],
         ticketLocation: [ "Where to purchase tickets", "Nashville, TN" ],
         image: [ "ImageURL", "url" ]
-    },
+    }
+
+
+
 /*
     {
         date: ["Date", ""],
@@ -30,20 +43,53 @@ const newsFeed = document.querySelector( ".newsfeed-main" );
 
 console.log( Object.keys( newsRecords[0] ) );
 
+function addArticleElement( article, item, element, element_class ) {
+    const articleElement = document.createElement( element );
+    articleElement.className = element_class;
+    articleElement.innerHTML = item;
+    article.appendChild( articleElement );
+}
+
+
+/*
+function addArticleImage ( article, item, element, element_class ) {
+    const articleElement = document.createElement( 'div' );
+    articleElement.className = element_class;
+    //articleElement.innerHTML = item;
+    articleElement.src = item;
+    article.appendChild( articleElement );
+}*/
+
+
+function addArticleImage( article, item, element, element_class ) {
+    const divElement = document.createElement( 'div' );
+    divElement.className="newsfeed-main__article__div";
+
+    const itemPics = document.createElement( element );
+    itemPics.src = item;
+    itemPics.alt = "News related image";
+    itemPics.style = "width: 100px; margin: .9em";
+    itemPics.className = element_class;
+
+    divElement.appendChild( itemPics );
+
+    article.appendChild( divElement );
+}
+
+
 function addArticle( record ) {
     const newsArticle = document.createElement( 'article' );
     newsArticle.className = "newsfeed-main__article";
 
-    const newsArticleH4 = document.createElement( 'h4' );
-    newsArticleH4.className = "newsfeed-main__article__h4";
-    newsArticleH4.innerHTML = record.date[1] + ", " + record.location[1];
-    newsArticle.appendChild( newsArticleH4 );
+    addArticleElement( newsArticle, record.date[1] + ", " + record.location[1], 'h4', 'newsfeed-main__article__h4' );
+    addArticleElement( newsArticle, record.title[1], 'h3', 'newsfeed-main__article__h4' );
+    addArticleElement( newsArticle, record.message[1], 'section', 'newsfeed-main__article__section' );
+    addArticleElement( newsArticle, record.typeOfEvent[0] + ": " + record.typeOfEvent[1], 'div', 'newsfeed-main__article__div' );
+    /*addArticleElement( newsArticle, record.collaborators[1], 'div', 'newsfeed-main__article__div' );*/
+    addArticleElement( newsArticle, record.ticketLocation[0] + ": " + record.ticketLocation[1], 'div', 'newsfeed-main__article__div' );
 
-    const newsArticleSection = document.createElement( 'section' );
-    newsArticleSection.className = "newsfeed-main__article__section";
-    newsArticleSection.innerHTML = record.message[1];
-    newsArticle.appendChild( newsArticleSection );
-   
+    addArticleImage( newsArticle, record.image[1], 'img', 'newsfeed-main__article__img' );
+
     newsFeed.appendChild( newsArticle );
 }
 
